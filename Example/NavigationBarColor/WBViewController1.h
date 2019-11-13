7a369b030d1bcba81c3fa963994bd346da11552a
wangbiao <304635659@qq.com>
2019-11-13 15:51:54 +0800

    first commit

---
+++ Example/NavigationBarColor/WBViewController1.h
@@ -0,0 +1,17 @@
+//
+//  WBViewController1.h
+//  NavigationBarColor_Example
+//
+//  Created by nana on 2019/11/13.
+//  Copyright © 2019 304635659@qq.com. All rights reserved.
+//
+
+#import "WBViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WBViewController1 : WBViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
